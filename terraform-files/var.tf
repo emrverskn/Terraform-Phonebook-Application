fbe3d3b223b578533e19d9a54a0b1b2e0c920aa7
merverskn <emrverskn@gmail.com>
2023-12-28 18:27:44 +0300

    files added

---
+++ terraform-files/var.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "****"
+}
+
+variable "git-token" {
+  default = "****"
+}
+
+variable "key-name" {
+  default = "****"
+}
+
+variable "hosted-zone" {
+  default = "****"
+}
