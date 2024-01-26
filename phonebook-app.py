cd78d0c2707d34de929bc4a09daecda2d0754003
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-26 22:10:26 +0300

    Update phonebook-app.py

--- phonebook-app.py
+++ phonebook-app.py
@@ -75,7 +75,6 @@ def update_person(name, number):
     return f'Phone record of {name.strip().title()} is updated successfully'
 
 
-
 def delete_person(name):
     query = f"""
     SELECT * FROM phonebook WHERE name like '{name.strip().lower()}';
